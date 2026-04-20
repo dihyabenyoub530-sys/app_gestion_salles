9d5ad9c0e24ccc30597f904db0b729179123161b
Dyhia <dihyabenyoub530@gmail.com>
2026-04-20 11:01:59 -0400

    Ajout des tests de la couche Service dans main

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ for s in liste:
     salle1 = Salle("A102", "Salle Test", "Laboratoire", 20)
     dao.insert_salle(salle1)
 
-    # Modifier une salle
+
     salle_modifiee = Salle("A102", "Salle Modifiée", "Classe", 30)
     dao.update_salle(salle_modifiee)
 
-    # Rechercher une salle
+
     s = dao.get_salle("A102")
     if s:
         print("Salle trouvée :")
@@ -31,13 +31,13 @@ for s in liste:
 
     print("-------------")
 
-    # Afficher toutes les salles
+
     liste = dao.get_salles()
     for salle in liste:
         salle.afficher_infos()
         print("-------------")
 
-    # Supprimer la salle test
+
     dao.delete_salle("A102")
     print("Salle A102 supprimée")
     from services.services_salle import ServiceSalle
@@ -45,17 +45,17 @@ for s in liste:
 
     service = ServiceSalle()
 
-    # Ajouter
+
     salle1 = Salle("B101", "Salle Service", "Classe", 35)
     succes, message = service.ajouter_salle(salle1)
     print(message)
 
-    # Modifier
+
     salle2 = Salle("B101", "Salle Service Modifiée", "Laboratoire", 40)
     succes, message = service.modifier_salle(salle2)
     print(message)
 
-    # Rechercher
+
     salle_trouvee = service.rechercher_salle("B101")
     if salle_trouvee:
         print("Salle trouvée :")
@@ -63,12 +63,12 @@ for s in liste:
 
     print("-------------")
 
-    # Afficher toutes les salles
+
     liste = service.recuperer_salles()
     for s in liste:
         s.afficher_infos()
         print("-------------")
 
-    # Supprimer
+
     service.supprimer_salle("B101")
     print("Salle B101 supprimée")
