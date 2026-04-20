dc76406255abfc6f09891a801ab7316aaa9e843b
Dyhia <dihyabenyoub530@gmail.com>
2026-04-20 10:45:49 -0400

    Ajout de la connexion MySQL et des premières méthodes DAO

---
+++ Data/dao_salle.py
@@ -0,0 +1,30 @@
+import mysql.connector
+import json
+from models.salle import Salle
+
+class DataSalle:
+
+    def get_connection(self):
+        with open("Data/config.json") as f:
+            config = json.load(f)
+        return mysql.connector.connect(**config)
+
+    def insert_salle(self, salle):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        query = "INSERT INTO salle (code, libelle, type, capacite) VALUES (%s, %s, %s, %s)"
+        values = (salle.code, salle.libelle, salle.type, salle.capacite)
+        cursor.execute(query, values)
+        conn.commit()
+        conn.close()
+
+    def get_salles(self):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM salle")
+        rows = cursor.fetchall()
+        salles = []
+        for r in rows:
+            salles.append(Salle(r[0], r[1], r[2], r[3]))
+        conn.close()
+        return salles
