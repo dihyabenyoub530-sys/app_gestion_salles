3f8f620e11a00d2267908d7968e05496f8f242b2
Dyhia <dihyabenyoub530@gmail.com>
2026-04-20 10:56:40 -0400

    Ajout des tests CRUD de la couche DAO dans main

--- main.py
+++ main.py
@@ -10,3 +10,33 @@ liste = dao.get_salles()
 for s in liste:
     s.afficher_infos()
     print("-------------")
+    from Data.dao_salle import DataSalle
+    from models.salle import Salle
+
+    dao = DataSalle()
+
+
+    salle1 = Salle("A102", "Salle Test", "Laboratoire", 20)
+    dao.insert_salle(salle1)
+
+    # Modifier une salle
+    salle_modifiee = Salle("A102", "Salle Modifiée", "Classe", 30)
+    dao.update_salle(salle_modifiee)
+
+    # Rechercher une salle
+    s = dao.get_salle("A102")
+    if s:
+        print("Salle trouvée :")
+        s.afficher_infos()
+
+    print("-------------")
+
+    # Afficher toutes les salles
+    liste = dao.get_salles()
+    for salle in liste:
+        salle.afficher_infos()
+        print("-------------")
+
+    # Supprimer la salle test
+    dao.delete_salle("A102")
+    print("Salle A102 supprimée")
